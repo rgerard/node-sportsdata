1e4e24b79eb9c5c870b58b970e6a4d2a40a2c180
Ryan Gerard <ryan.gerard@gmail.com>
2012-11-19 09:31:44 -0800

    Added the remaining NBA API calls

--- index_nba.js
+++ index_nba.js
@@ -9,10 +9,52 @@ function getSeasonSchedule(callback) {
     createRequest(url, callback);
 }
 
+function get3DaySchedule(callback) {
+    var url = urlHelper.get3DayScheduleUrl();
+    createRequest(url, callback);
+}
+
+function getStandings(callback) {
+    var url = urlHelper.getStandingsUrl();
+    createRequest(url, callback);
+}
+
+function getRosters(callback) {
+    var url = urlHelper.getRostersUrl();
+    createRequest(url, callback);
+}
+
+function getGameScoreAndStats(gameid, callback) {
+    var url = urlHelper.getGameScoreAndStatsUrl(gameid);
+    createRequest(url, callback);
+}
+
+function getPlayByPlay(gameid, callback) {
+    var url = urlHelper.getPlayByPlayUrl(gameid);
+    createRequest(url, callback);
+}
+
+function getEventsGloassary(callback) {
+    var url = urlHelper.getEventsGloassaryUrl();
+    createRequest(url, callback);
+}
+
+function getSeasonalStats(teamid, callback) {
+    var url = urlHelper.getSeasonalStatsUrl(teamid);
+    createRequest(url, callback);
+}
+
+function getStatus(callback) {
+    var url = urlHelper.getStatusUrl();
+    createRequest(url, callback);
+}
+
+
 function createRequest(url, callback) {
     request(url, function (error, response, body) {
 
         if (response.statusCode == 200) {
+
             // Parse the XML to JSON
             parser.parseString(body, function (err, result) {
                 callback(error, result);
@@ -30,5 +72,37 @@ module.exports = {
 
     getSeasonSchedule: function(callback) {
         return getSeasonSchedule(callback);
+    },
+
+    get3DaySchedule: function(callback) {
+        return get3DaySchedule(callback);
+    },
+
+    getStandings: function(callback) {
+        return getStandings(callback);
+    },
+
+    getRosters: function(callback) {
+        return getRosters(callback);
+    },
+
+    getGameScoreAndStats: function(gameid, callback) {
+        return getGameScoreAndStats(gameid, callback);
+    },
+
+    getPlayByPlay: function(gameid, callback) {
+        return getPlayByPlay(gameid, callback);
+    },
+
+    getEventsGloassary: function(callback) {
+        return getEventsGloassary(callback);
+    },
+
+    getSeasonalStats: function(teamid, callback) {
+        return getSeasonalStats(teamid, callback);
+    },
+
+    getStatus: function(callback) {
+        return getStatus(callback);
     }
-}
+};
