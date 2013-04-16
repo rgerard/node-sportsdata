ecaf8641f963a004dfec28a77b13eb9d4e1af54d
Ryan Gerard <ryan.gerard@gmail.com>
2013-04-15 23:08:19 -0700

    Cleanup known MLB calls

--- index_mlb.js
+++ index_mlb.js
@@ -40,37 +40,6 @@ function getStandings(callback) {
     createRequest(url, callback);
 }
 
-function getRosters(callback) {
-    var url = urlHelper.getRostersUrl();
-    createRequest(url, callback);
-}
-
-function getGameScoreAndStats(gameid, callback) {
-    var url = urlHelper.getGameScoreAndStatsUrl(gameid);
-    createRequest(url, callback);
-}
-
-function getPlayByPlay(gameid, callback) {
-    var url = urlHelper.getPlayByPlayUrl(gameid);
-    createRequest(url, callback);
-}
-
-function getEventsGloassary(callback) {
-    var url = urlHelper.getEventsGloassaryUrl();
-    createRequest(url, callback);
-}
-
-function getSeasonalStats(teamid, callback) {
-    var url = urlHelper.getSeasonalStatsUrl(teamid);
-    createRequest(url, callback);
-}
-
-function getStatus(callback) {
-    var url = urlHelper.getStatusUrl();
-    createRequest(url, callback);
-}
-
-
 function createRequest(url, callback) {
     request(url, function (error, response, body) {
 
@@ -106,29 +75,5 @@ module.exports = {
 
     getStandings: function(callback) {
         return getStandings(callback);
-    },
-
-    getRosters: function(callback) {
-        return getRosters(callback);
-    },
-
-    getGameScoreAndStats: function(gameid, callback) {
-        return getGameScoreAndStats(gameid, callback);
-    },
-
-    getPlayByPlay: function(gameid, callback) {
-        return getPlayByPlay(gameid, callback);
-    },
-
-    getEventsGloassary: function(callback) {
-        return getEventsGloassary(callback);
-    },
-
-    getSeasonalStats: function(teamid, callback) {
-        return getSeasonalStats(teamid, callback);
-    },
-
-    getStatus: function(callback) {
-        return getStatus(callback);
     }
 };
